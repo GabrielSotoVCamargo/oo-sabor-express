fc6b45c3b71da2a55ed7dac90d1a929e59421eb6
GabrielSotoVCamargo <gabrielsotocamargo@gmail.com>
2025-04-23 15:40:51 -0300

    git add .

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,19 +2,30 @@ class Restaurante:
     restaurantes = []
     #metodo construtor 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     #metodo especial
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+#criação de property para o atributo ativo.
+# Além disso não é necessario criar toda hora property para cada atributo
+    @property
+    def ativo(self):
+        return "💲" if self._ativo else "🔴" 
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
